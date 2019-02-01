c5a05bb9fcfa5bd6d5fc8dc544aebf32736c68d8
Rafeal Chen <winduanckc@gmail.com>
2019-02-01 15:52:19 +0800

    add header

--- products.py
+++ products.py
@@ -4,6 +4,7 @@ while True:
 	if name == 'q':
 		break
 	price = input('請輸入商品價格: ')
+	price = int(price)
 	products.append([name, price])
 print(products)
 
@@ -12,3 +13,15 @@ print(products[0][0])
 
 for p in products:
 	print(p[0], "的價格是", p[1])
+
+##字串可以做+跟*
+
+
+with open('products.csv', 'w', encoding='UTF-8') as f: # open只是打開而已
+	f.write('商品,價格\n')
+	for p in products:
+		f.write(p[0]+','+str(p[1])+'\n') # 這行才是真正的寫入
+
+
+# csv 檔通常以逗點','做為區隔。
+# \n為換行
